eef9ab767205b61a06137f1d9d400daba7753d1a
João Maruyama <Joao.Maruyama@alunos.ifsuldeminas.edu.br>
2026-05-14 11:36:10 -0300

    Teste de Sincronização

--- src/main/java/br/edu/tds/ecommerce/App.java
+++ src/main/java/br/edu/tds/ecommerce/App.java
@@ -21,7 +21,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("telaCadastroProdutos"), 700, 500);
+        scene = new Scene(loadFXML("TelaLogin"), 700, 500);
         stage.setScene(scene);
         stage.show();
     }
